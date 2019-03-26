8f4b9d46889128ed802990fb21c1621ea12d170a
Bruno Fantin <bruno.fantin@gmail.com>
2019-03-26 13:27:34 -0300

    inicio do projeto

---
+++ src/main/java/teste/brasil/prev/TesteBrasilPrevApplication.java
@@ -0,0 +1,13 @@
+package teste.brasil.prev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteBrasilPrevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesteBrasilPrevApplication.class, args);
+	}
+
+}
